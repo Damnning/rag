d7eddedbbe360062a1294a7467b7f08aa814d58e
Damnning <Danning64@gmail.com>
2025-07-21 11:45:33 +0300

    feat: project structure

---
+++ app/src/infrastructure/document_retriever/document_retriever.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class DocumentFormat(Enum):
+    PDF = '.pdf'
+    DOCX = '.docx'
+    HTML = '.html'
+    TXT = '.txt'
+
+
+class DocumentRetriever:
+    def __init__(self, path: str, format: DocumentFormat):
+        self.path = path
+        self.format = format
+
+    def retrieve(self):
+
